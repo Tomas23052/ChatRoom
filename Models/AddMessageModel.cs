1521fb5312bcb43dec25e22d73e65ec8e7418509
Xyrsto <73367973+Xyrsto@users.noreply.github.com>
2023-11-24 22:01:48 +0000

    Removed useless files

--- Models/AddMessageModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace SignalRTeste.Models
-{
-    public class AddMessageModel : PageModel
-    {
-    }
-}
--- Models/Users.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SignalRTeste.Models
-{
-    public class Users
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string password { get; set; }
-    }
-}
